60e10d6380f0e7cdff31791ee098d71a63f98288
velen2004 <151418735+velen2004@users.noreply.github.com>
2025-11-21 12:56:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function bookTrip(location) {
+    alert("Thank you! You have shown interest in visiting " + location + ".");
+    
+    // Change background color slightly to show interaction
+    document.body.style.backgroundColor = "#e0e0e0";
+    
+    console.log("User clicked on: " + location);
+}
